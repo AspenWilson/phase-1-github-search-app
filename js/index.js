224c21c05751d5b2ec4a7d716d7ffae425449b3e
Aspen Wilson <aspen.schell@gmail.com>
2023-03-06 18:10:39 -0800

    updated while doing blog

--- js/index.js
+++ js/index.js
@@ -1,77 +1,72 @@
+//Constants
 const form = document.getElementById('github-form')
+const repoList= document.getElementById('repos-list')
+const userList = document.querySelector('#user-list')
+
+//Event Listener
 form.addEventListener('submit', (event) => {
     event.preventDefault()
-    fetch(`https://api.github.com/search/users?q=${event.target[0].value}`)
+})
+
+//User Keyword Search
+function userSearch () {
+    fetch(`https://api.github.com/search/users?q=${form[0].value}`)
+        .then(resp => resp.json())
+        .then(resp =>  {
+            userList.innerHTML =''
+            repoList.innerHTML= ''
+            resp.items.map(item => {
+                const li = document.createElement('li')
+                li.className='li-item'
+                li.innerHTML = `
+                <h2>${item.login}</h2>
+                <img src=${item.avatar_url} />
+                <button id='btn1'>User Repository List</button><br>
+                <a class='button' href=${href=item.html_url}>See user profile</a>`
+                li.querySelector('#btn1').addEventListener('click', e => userRepos(item.login,e))
+                userList.append(li)
+            })
+        })
+        form.reset()
+    }
+
+//Repo Keyword Search
+function keywordSearch () {
+    fetch(`https://api.github.com/search/repositories?q=${form[0].value}`)
     .then(resp => resp.json())
     .then(resp =>  {
-        const userList = document.querySelector('#user-list')
         userList.innerHTML =''
-        const repoList= document.getElementById('repos-list')
         repoList.innerHTML= ''
         resp.items.map(item => {
             const li = document.createElement('li')
-            const h2 = document.createElement('h2')
-            h2.textContent = item.login
-            const img= document.createElement('img')
-            img.src = item.avatar_url
-            const btn1= document.createElement('button')
-            btn1.innerText = 'User Repository List'
-            btn1.addEventListener('click', e => userRepos(item.login,e))
-            const a=document.createElement('a')
-            a.innerText='See user profile'
-            a.className='button'
-            a.href=item.html_url 
-            li.append(h2,a,img,btn1)
+            li.className = 'li-item'
+            let owner = item.owner.login
+            li.innerHTML = `
+            <h2>${item.name}</h2>
+                <h3>Owner</h3>
+                <h4>${owner}</h4>
+                <img src=${item.owner.avatar_url} />
+                <button id='btn'>Owners Full Repository List</button>`
+            li.querySelector('#btn').addEventListener('click', e => userRepos(owner,e))
             userList.append(li)
         })
     })
     form.reset()
-})
+}
 
+//Repo Search and Display
 function userRepos(username,e) {
-    const repoList= document.getElementById('repos-list')
     repoList.innerHTML=''
     e.preventDefault()
     fetch(`https://api.github.com/users/${username}/repos`)
     .then(resp => resp.json())
     .then(resp => resp.map(repo => {
         const li = document.createElement('li')
-        const h1 = document.createElement('h1')
-        h1.textContent = repo.name
-        const repoList= document.getElementById('repos-list')
-        li.append(h1)
+        li.innerHTML= `
+        <h1>${repo.name}</h1>`
         repoList.append(li)
     })  
     )
 }
-const formKeyword = document.getElementById('github-form-keyword')
-formKeyword.addEventListener('submit', (event) => {
-    event.preventDefault()
-    fetch(`https://api.github.com/search/repositories?q=${event.target[0].value}`)
-    .then(resp => resp.json())
-    .then(resp =>  {
-        const userList = document.querySelector('#user-list')
-        userList.innerHTML =''
-        const repoList= document.getElementById('repos-list')
-        repoList.innerHTML= ''
-        resp.items.map(item => {
-            const li = document.createElement('li')
-            const h2 = document.createElement('h2')
-            const h3=document.createElement('h3')
-            const h4=document.createElement('h4')
-            h2.textContent = item.name
-            let owner = item.owner.login
-            h3.textContent = `Owner:`
-            h4.textContent= owner
-            const img= document.createElement('img')
-            img.src = item.owner.avatar_url
-            const btn= document.createElement('button')
-            btn.innerText = 'Owners Full Repository List'
-            btn.addEventListener('click', e => userRepos(owner,e))
-            li.append(h2,h3,h4,img,btn)
-            userList.append(li)
-        })
-    })
-    form.reset()
-})
+
 
